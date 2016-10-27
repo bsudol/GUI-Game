ce76be6958925053e4192532b7bb6c76b189b006
bsudol <basiaasudol@gmail.com>
2016-10-27 10:53:02 -0400

    work on minmax

--- src/controller/MinMaxAI.java
+++ src/controller/MinMaxAI.java
@@ -110,17 +110,29 @@ public abstract class MinMaxAI extends Controller {
 		}
 		
 		//otherwise get a list of new game states for locations
-		for (Location k : available) {
-			//this gets the score for the player
-			int result = 0;
-			int count  = 0;
-			for (Line s : Line.ALL_LINES) {
-				int score = score(b,p,s);
-				result += score;
-			}
-			
-		}
+		//so we need to say basically, if we put an "X" here, what will the game state be, and/or what will the score be
+		List<Integer> allthescores = new ArrayList<>(); 
+		List<Location> goodmoves = new ArrayList<>(); 
 
+		
+		for (Location k : available) {
+            if (turn == 1) { //X's turn select the highest from below minimax() call
+                g.submitMove(p, k);
+                int currentScore = minimaxalg(g, p, depth + 1, 2);
+                allthescores.add(currentScore);
+
+                if (depth == 0) {
+                    goodmoves.add(k);
+                }  
+            } 
+			else if (turn == 2) {//O's turn select the lowest from below minimax() call
+                g.submitMove(p.opponent(), k); 
+                allthescores.add(minimaxalg(g, p, depth + 1, 1));
+            }
+		}
+		
+		//do i need another method to get max and min here.
+        return (turn == 1 ? allthesccores.max : allthescores.min);
 	}
 	
 	/**
@@ -129,59 +141,9 @@ public abstract class MinMaxAI extends Controller {
 	 */
 	protected @Override Location nextMove(Game g) {
 		// TODO Auto-generated method stub
-		//might want to add a move or int parameter to call from minimaxalg
+		
+		//call minmaxalg here and return best move, but not finished
 		
 	}
 	
-}
- * pieces are; by restricting our search to only part of the board, we can
- * reduce the space considerably.
- * 
- * <p>A second strategy is that we can look only a few moves ahead instead of
- * planning all the way to the end of the game.  This requires us to be able to
- * estimate how "good" a given board looks for a player.
- * 
- * <p>This class implements the minimax algorithm with support for these two
- * strategies for reducing the search space.  The abstract method {@link
- * #moves(Board)} is used to list all of the moves that the AI is willing to
- * consider, while the abstract method {@link #estimate(Board)} returns
- * the estimation of how good the board is for the given player.
- */
-public abstract class MinMaxAI extends Controller {
-
-	/**
-	 * Return an estimate of how good the given board is for me.
-	 * A result of infinity means I have won.  A result of negative infinity
-	 * means that I have lost.
-	 */
-	protected abstract int estimate(Board b);
-	
-	/**
-	 * Return the set of moves that the AI will consider when planning ahead.
-	 * Must contain at least one move if there are any valid moves to make.
-	 */
-	protected abstract Iterable<Location> moves(Board b);
-	
-	/**
-	 * Create an AI that will recursively search for the next move using the
-	 * minimax algorithm.  When searching for a move, the algorithm will look
-	 * depth moves into the future.
-	 *
-	 * <p>choosing a higher value for depth makes the AI smarter, but requires
-	 * more time to select moves.
-	 */
-	protected MinMaxAI(Player me, int depth) {
-		super(me);
-		// TODO Auto-generated method stub
-		throw new NotImplementedException();
-	}
-
-	/**
-	 * Return the move that maximizes the score according to the minimax
-	 * algorithm described above.
-	 */
-	protected @Override Location nextMove(Game g) {
-		// TODO Auto-generated method stub
-		throw new NotImplementedException();
-	}
 }
