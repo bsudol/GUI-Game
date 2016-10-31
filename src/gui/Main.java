f7463e5be0edfa57da84ea127e0a198753a8d269
bsudol <basiaasudol@gmail.com>
2016-10-31 11:16:26 -0400

    added functional gui - drew mera

--- src/gui/Main.java
+++ src/gui/Main.java
@@ -1,18 +1,13 @@
-/* NetId(s): bas334, dnm54. */
+/* NetId(s): dnm54, bas334.*/
 
 package gui;
 
-import clui.BoardPrinter;
-import clui.ConsoleController;
-import controller.Controller;
-import controller.DumbAI;
-import controller.RandomAI;
-import controller.SmartAI;
-import model.Game;
-import model.NotImplementedException;
-import model.Player;
+import controller.*;
+import model.*;
+import model.Board.State;
 import java.awt.*;
 import java.awt.event.*;
+import java.util.ArrayList;
 import javax.swing.*;
 import javax.swing.border.LineBorder;
 
@@ -22,32 +17,67 @@ public class Main extends JFrame{
 	//JLabel label;
 	Controller playerX, playerO;
 	JFrame gameFrame;
+	Game guigame;
+	Board guiBoard;
+	ArrayList<Square> squares = new ArrayList<Square>();
 	
 	private class okClick implements ActionListener {
 
 		@Override
 		public void actionPerformed(ActionEvent e) {
-			playerX = createController((String) cbPlayerX.getSelectedItem(), Player.X);
-			playerO = createController((String) cbPlayerO.getSelectedItem(), Player.O);
+			if (((String) cbPlayerX.getSelectedItem()) != "User") {
+				playerX = createController((String) cbPlayerX.getSelectedItem()
+						, Player.X); 
+				guigame.addListener(playerX);
+			}
+			
+			if (((String)cbPlayerO.getSelectedItem()) != "User") {	
+				playerO = createController((String) cbPlayerO.getSelectedItem()
+						, Player.O); 
+				guigame.addListener(playerO);
+			}
+			
 			gameFrame.setVisible(true);
 			setVisible(false);
-			
 		}
 		
 	}
 	
+	public class BoardPrinter implements GameListener {
+
+		@Override
+		public void gameChanged(Game g) {
+			// TODO Auto-generated method stub
+			Board b = g.getBoard();
+			Square s;
+			for (int i = 0; i < 9; i++) {
+				for (int j = 0; j < 9; j++) {
+					if (b.get(i, j) != null) {
+						s = squares.get(i*9 + j);
+						s.repaint();
+					}
+						
+				}
+			}
+		}
+		
+	}
 	
 	private class mouseOver implements MouseListener {
 		@Override
 		public void mouseClicked(MouseEvent e) {
 			// TODO Auto-generated method stub
-			Square labelClicked = (Square) e.getSource();
-			labelClicked.selected = true;
-			labelClicked.repaint();
+			Square panelClicked = (Square) e.getSource();
+			if (panelClicked.selected == false) {
+				//panelClicked.paintImmediately(0,0,
+					//	panelClicked.getWidth(), panelClicked.getHeight());
+				panelClicked.selected = true;
+			}
 			
-			/*if (p == Player.X) p = Player.O;
-			else p = Player.X;*/
 			
+			guigame.submitMove(p, panelClicked.loc);
+			
+			p = guigame.nextTurn();
 			
 		}
 
@@ -67,66 +97,104 @@ public class Main extends JFrame{
 		public void mouseEntered(MouseEvent e) {
 			// TODO Auto-generated method stub
 			
-			Square labelClicked = (Square) e.getSource();
-			labelClicked.inSquare = true;
-			if (!labelClicked.selected)
-				labelClicked.repaint();
+			Square panelClicked = (Square) e.getSource();
+			panelClicked.inSquare = true;
+			if (!panelClicked.selected)
+				panelClicked.repaint();
 		}
 
 		@Override
 		public void mouseExited(MouseEvent e) {
 			// TODO Auto-generated method stub
-			Square labelClicked = (Square) e.getSource();
-			labelClicked.inSquare = false;
-			if (!labelClicked.selected)
-				labelClicked.repaint();
+			Square panelClicked = (Square) e.getSource();
+			panelClicked.inSquare = false;
+			if (!panelClicked.selected)
+				panelClicked.repaint();
 		}
-		
 	}
 	
 	public class Square extends JButton {
 		private boolean inSquare = false;
 		private boolean selected = false;
-		public Square() {
+		
+		Location loc;
+		
+		public Square(int i, int j) {
+			loc = new Location(i,j);
 			setPreferredSize(new Dimension(10,10));
 			setOpaque(true);
 			setBorder(new LineBorder(Color.blue));
 			addMouseListener(new mouseOver());
+			squares.add(this);
+		}
+		
+		public Square getSquare(int i, int j) {
+			return squares.get(i*9 +j);
+		}
+		
+		public ArrayList<Square> getSquareList() {
 			
+			return squares;
 		}
 		
 		@Override
 		public void paint(Graphics g) {
 			super.paint(g);
+			boolean select;
+			select = guigame.getBoard().get(loc) != null;
 			g.setFont(new Font("", Font.PLAIN, 50));
-			//g.drawString("X", 40, 55);
-			if (inSquare || selected) {
-				if (p == Player.O)
-					g.drawString("O", 40, 55);
-				else 
+			if (inSquare && !select) {
+				if (guigame.nextTurn() == Player.X)
 					g.drawString("X", 40, 55);
-				//g.drawArc(20, 10, getWidth() - 41, getHeight() - 21, 0, 360);
+				else 
+					g.drawString("O", 40, 55);
 			}
-			else if (!selected)
+			if (select) {
+				if (guigame.getBoard().get(loc) == Player.X)
+					g.drawString("X", 40, 55);
+				else 
+					g.drawString("O", 40, 55);
+			}
+			else if (!select)
 				g.dispose();
-			//else if (p == Player.O && inSquare)
-				//g.drawString("O", 0, 0);
+			
+			if (guigame.getBoard().getState() == State.HAS_WINNER) {
+				paintWinners();
+			}
+		}
+		
+		public void paintWinners() {
+			Board b = guigame.getBoard();
+			Line winners = b.getWinner().line;
+			Square win;
+			for (int i = 0; i < 9; i++) {
+				for (int j = 0; j < 9; j++) {
+					if (winners.contains(i, j)) {
+						win = getSquare(i,j);
+						win.setBackground(Color.GREEN);
+					}
+						
+				}
+			}
 		}
 	}
 	
+	
 	public Main() {
 		//start a new game
-		Game guigame = new Game();
+		guigame = new Game();
 		//start up the JFrame for the game
 	    gameFrame = new JFrame();
 	    JPanel gamePanel = new JPanel();
-	    //gameFrame.setLayout(new FlowLayout());
 		gamePanel.setLayout(new GridLayout(9,9));
-
+	
+		
 		//add all 81 buttons into the grid layout for the 'board'
-		for (int count = 0; count < 81; count ++) {
-			gamePanel.add(new Square());
-			
+		for (int i = 0; i < 9; i ++) {
+			for (int j = 0; j < 9; j ++) {
+				Square s = new Square(i, j);
+				gamePanel.add(s);	
+			}
 		}
 		
 		//lay it out/make it go
@@ -161,40 +229,31 @@ public class Main extends JFrame{
 	    add(cbPlayerX, BorderLayout.WEST);
 	    add(label, BorderLayout.CENTER);
 	    add(cbPlayerO, BorderLayout.EAST);
-	    
+
 	    JButton ok = new JButton("OK");
 	    p = Player.X;
 	    ok.addActionListener(new okClick());
 	    add(ok, BorderLayout.SOUTH);
-	    
+	   
 		setPreferredSize(new Dimension(300,150));
 		pack();
 		setVisible(true);
-		
-		
-	    //dispose();
-		
-		
-		guigame.addListener(playerX);//i dont exactly know what these do
-		guigame.addListener(playerO);
-		
-		//need some code here to print the new GUI picture when it changes	
-	    
+
+		guigame.addListener(new BoardPrinter());
 	}
 	
 	public Controller createController(String selection, Player p){
 		switch(selection) {
 		case "User":
-			System.out.println("user");
-			return new ConsoleController(p);
-		case "Dumb Computer":
+			return null;
+		case "Dumb AI":
 			return new DumbAI(p);
-		case "Smart Computer":
+		case "Random AI":
 			return new RandomAI(p);
-		case "Random Computer":
+		case "Smart AI":
 			return new SmartAI(p);
 		default:
-			return new ConsoleController(p);
+			return null;
 			
 		}
 		
