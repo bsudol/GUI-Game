66f621f59876c4fab9d3bf8e3b9b0e0742edc0f1
bsudol <basiaasudol@gmail.com>
2016-10-31 12:41:56 -0400

    improvements on GameTree

--- src/controller/GameTree.java
+++ src/controller/GameTree.java
@@ -7,70 +7,83 @@ import model.*;
 public class GameTree {
 	
 	private Game g = new Game();
-	private Board board = g.getBoard();
-	
-	private int score;
-	
+	private Board board = g.getBoard(); //each node will have its own copy of the board
 	private int depth;
+	private int score; //score associated with this copy of board
 	public static int maxDepth = 0;
-	
 			
 	private ArrayList<GameTree> children = new ArrayList<GameTree>();
 	
-	public GameTree(Player p, int d) {
+	public GameTree(Player p, int d, int s) {
 		depth = d;
 		final List<Location> available = new ArrayList<Location>();
-		Game game1 = null;
-		Board board1 = null;
 		
-		score = 0;
-		
+		score = s; //score associated with that board
+	}
+	public GameTree dfs(GameTree n, int d, Player p) {
+		//create children of node
+		n.children = CreateChildren(p);
+		//depth first search through children
+		for (GameTree each: n.children) {
+			//start with children closest to non-null entries THIS REQUIRES RANKNIG
+
+			if (depth > d) {
+				return each; //return because you hit max depth
+			}
+			if (board.getState().HAS_WINNER) { //need to fix how this is accessed
+				if (player.current == lost) {
+					score = -1000000;
+					//do i return a node here even though its not a good path to take?
+				}
+				else {
+					score = 1000000;
+					return each;
+				}
+			}
+			dfs(n, d, p);
+		}
+		return null;
+	}
+	private ArrayList<GameTree> CreateChildren(Player p) {
+
+		List<Location> available = new ArrayList<Location>();
 		// find available moves
 		for (Location loc : Board.LOCATIONS) {
 			if (board.get(loc) == null) {
 				available.add(loc);
 			}
 		}
-				
-		//head = empty board
+
 		for (Location k : available) {
-			game1 = new Game();
-			board1 = game1.getBoard();
-			
-			game1.submitMove(p, k);
-	
-			GameTree child = new GameTree(p); //this will overflow
+			g.submitMove(p, k);
+			score = score(board, p); //get the score of this configuration
+			GameTree child = new GameTree(p, depth, score); //this will overflow, need to move this into method
 			children.add(child);
 		}
-	}
-	public int dfs(GameTree n, int d) {
-		//create children of node
-		n.children = CreateChildren();
-		//depth first search through chilrden
-		for (GameTree n: available) {
-			//start with children closest to non-null entries
-			//
-			if (depth > d) {
-				// return because you exceeded max depth
-			}
-			if (board.getState().HAS_WINNER) {
-				// return because there's a winner, set g = infinity
-				if (player.current == lost) {
-					//score = -infinity
-				}
-				else {
-					//score = infinity
-				}
-			}
-			dfs(n);
-		}
-		return null;
-	}
-	private ArrayList<GameTree> CreateChildren() {
-		// TODO Auto-generated method stub
-		throw new NotImplementedException();
-	}
 		
+		return children; //does this return the right thing
+	}
+
+	/** return the positive score for player p */
+	public int score(Board b, Player p) {
+		int result = 0;
+		int count  = 0;
+		for (Line s : Line.ALL_LINES) {
+			int score = score(b,p,s);
+			result += score;
+		}
+		return result;
+	}
+	
+	/** Return the score for player p, for the line s. */
+	public int score(Board b, Player p, Line s) {
+		int result = 1;
+		for (Location loc : s) {
+			if (b.get(loc) == p)
+				result *= 10;
+			if (b.get(loc) == p.opponent())
+				return 0;
+		}
+		return result/10;
 	}
   }
-}
